51e7d91c964c60290ec0910e7eabcc92ef77e6d3
Tharahan Tharmaraja <tharahan04@hotmail.com>
2023-03-28 16:17:06 +0100

    added_deposit/withdrawals

--- BankAccount/src/main/java/org/example/BankAccount.java
+++ BankAccount/src/main/java/org/example/BankAccount.java
@@ -3,22 +3,48 @@ package org.example;
 import java.time.LocalDate;
 
 public class BankAccount {
+//    Properties
     private String firstName;
     private String lastName;
-
     private LocalDate dateOfBirth;
-
     private int accountNumber;
     private double balance;
 
-//    Constructor
+
+    //    Constructor
     public BankAccount(String inputfirstName, String inputlastName, LocalDate dateOfBirth, int accountNumber){
-        this.firstName = inputfirstName;
-        this.lastName = inputlastName;
-        this.dateOfBirth = dateOfBirth;
-        this.accountNumber = accountNumber;
-        this.balance = 0;
+            this.firstName = inputfirstName;
+            this.lastName = inputlastName;
+            this.dateOfBirth = dateOfBirth;
+            this.accountNumber = accountNumber;
+            this.balance = 0;
+    }
+
+
+    //        Methods
+            public String generateStatement(){
+                return "Hello " + this.firstName +" " +  this.lastName + "! Date of Birth : " + this.dateOfBirth + ". Account Number : " + this.accountNumber + "." ;
+
+            }
+//     Deposit
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }                             //display full balance
+
+//    Withdrawal 
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
-}
+
+
--- BankAccount/src/main/java/org/example/Main.java
+++ BankAccount/src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        BankAccount account1
+//        BankAccount account1 = new BankAccount("Tharahan", "Tharmaraja", LocalDate.of(2000,3,4), 12345678);
+
 
     }
 }
